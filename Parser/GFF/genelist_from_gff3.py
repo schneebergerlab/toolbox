1a2d34a0ede03180e43d630e6beb8ccbd7a4e10e
Manish Goel <goel@pc-t7-095.mpipz.mpg.de>
2018-09-04 14:08:10 +0200

    added genelist_from_gff3

--- Parser/GFF/genelist_from_gff3.py
+++ Parser/GFF/genelist_from_gff3.py
@@ -12,16 +12,11 @@ def getGenes(args):
     else:
         fin.to_csv(args.o.name,index = False, sep ="\t")
         
-       
-
-
-
 if __name__ == "__main__":
     parser = ap.ArgumentParser(formatter_class=ap.ArgumentDefaultsHelpFormatter)
     parser.set_defaults(func=getGenes)
     parser.add_argument("gff", help="Input .gff3 file", type = ap.FileType('r'))
     parser.add_argument("-o",help="output file name. default stdout", type = ap.FileType('w'))    
     args = parser.parse_args()
-    print(args)
     args.func(args)
 
