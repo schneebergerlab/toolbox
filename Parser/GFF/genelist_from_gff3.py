0efbfdfac1fa2b2d121f17c28a5618e991a51016
Manish Goel <goel@pc-t7-095.mpipz.mpg.de>
2018-09-06 14:14:18 +0200

    also gives gene name

--- Parser/GFF/genelist_from_gff3.py
+++ Parser/GFF/genelist_from_gff3.py
@@ -5,18 +5,23 @@ import argparse as ap
 def getGenes(args):
     from pandas import read_table
     fin = read_table(args.gff.name, header = None, comment="#")
-    fin = fin.loc[fin[2] == "gene",[0,3,4]]
-    fin.columns = ["chr","start","end"]
+    fout = fin.loc[fin[2] == "gene",[0,3,4]]
+    fout.columns = ["chr","start","end"]
+    if args.n:
+        info = fin.loc[fin[2] == "gene"][8]
+        info = [i.split(";")[1].split("=")[1].split("_")[1] if "Name" in i.split(";")[1] else "ERROR" for i in info]
+        fout["gene_name"] = info
     if args.o == None:
-        print(fin)
+        print(fout)
     else:
-        fin.to_csv(args.o.name,index = False, sep ="\t")
+        fout.to_csv(args.o.name,index = False, sep ="\t")
         
 if __name__ == "__main__":
     parser = ap.ArgumentParser(formatter_class=ap.ArgumentDefaultsHelpFormatter)
     parser.set_defaults(func=getGenes)
     parser.add_argument("gff", help="Input .gff3 file", type = ap.FileType('r'))
-    parser.add_argument("-o",help="output file name. default stdout", type = ap.FileType('w'))    
+    parser.add_argument("-o",help="output file name. default stdout", type = ap.FileType('w'))
+    parser.add_argument("-n",help="get gene_name too", action="store_true", default= False)
     args = parser.parse_args()
     args.func(args)
 
